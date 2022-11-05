089713e0e57e35324574d7764a48fbf6185b0512
Rishi01Prince <117525650+Rishi01Prince@users.noreply.github.com>
2022-11-05 19:52:17 +0530

    Add files via upload

---
+++ Hotstar_Disney_hover.js
@@ -0,0 +1,13 @@
+//Video and images just below slider
+
+const clip =  document.querySelectorAll('.card-video');
+for(let i =0 ; i<clip.length ; i++){
+  clip[i].addEventListener('mouseenter',function(e){
+    clip[i].play();
+  })
+
+  clip[i].addEventListener('mouseout',function(e){
+    clip[i].pause();
+  })
+  
+}
